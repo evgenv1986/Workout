5bff373e4aed4c233e046e88e0f4de856aadc588
evgeny <evgenv2015@gmail.com>
2025-06-15 11:46:27 +0700

    Снова отказался от использования джанго формы в представлении, в пользу обработки данных из request.post запроса. Затем создаю объект ExerciseStep  из полученных данных из формы

--- workout_django/excercise_execution/views.py
+++ workout_django/excercise_execution/views.py
@@ -1,40 +1,39 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from Exercise.Step import ExerciseStep
-
-
+from Exercise.Step.ExerciseStep import ExerciseStep
 
 from .forms import ExerciseExecuteForm
 
 def exercise_execute(request):        
     if request.method == 'GET':
         exercise_title = "Отжимания"
-        form = ExerciseExecuteForm (exercise_title = exercise_title)
+        # form = ExerciseExecuteForm (initial={'exercise_title': 'Отжимания'})
         return render (
             request,
             'execution/workout/exercise/excercise2.html',
-            {'form': form}
+            # {'form': form},
+            {'exercise_title' : "Отжимания"}
     )
         
     if request.method == 'POST':
-        form = ExerciseExecuteForm(request.POST)
-        if form.is_valid():
-            request.session['reps'] = form.cleaned_data['reps']
-            reps = request.session['exercise_reps']
-            title = form.cleaned_data['exercise_title']
-            request.session['exercise_title'] = title
+        # form = ExerciseExecuteForm(request.POST)
+        # if form.is_valid():
+            # request.session['reps'] = form.cleaned_data['reps']
+            # reps = request.session['exercise_reps']
+            # title = form.cleaned_data['exercise_title']
+            # request.session['exercise_title'] = title
             
         
         title = request.POST.get('exercise_title')
-        reps = request.POST.get('exercise_reps')
-        if not reps:
-            return HttpResponse("Пожалуйста, выберите количество повторений")
+        request.session['exercise_title'] = title
+        reps = request.POST.get('reps')
+        request.session['reps'] = reps
+        
+        step : ExerciseStep = ExerciseStep (title, reps)
+        # if not reps:
+            # return HttpResponse("Пожалуйста, выберите количество повторений")
     
-    step : ExerciseStep = ExerciseStep (title, reps)
-    
-    return render (
-        request,
-        'execution/workout/exercise/excercise2.html',
-    )
+        return HttpResponse (
+            f'step = {step}')
     
